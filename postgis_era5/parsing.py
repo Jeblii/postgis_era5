11a905a1c7d78956de0897bc2557e559199d02f9
Jeffrey Tsang <jeffreytsang@Jeffreys-MacBook-Pro.local>
2022-02-11 14:01:08 +0100

    parser for daily norms

--- postgis_era5/parsing.py
+++ postgis_era5/parsing.py
@@ -1,6 +1,9 @@
 import dataclasses as dc
 import datetime
+from xmlrpc.client import DateTime
 from postgis_era5.types import WGS84Point
+from typing import Mapping, List
+
 
 @dc.dataclass
 class DailyNorm:
@@ -43,3 +46,49 @@ class DailyNorm:
     total_evaporation_mean: float
     total_evaporation_max: float
 
+
+def parse_daily_norm(rows: Mapping[str, object]) -> List[DailyNorm]:
+    daily_norms = [
+        DailyNorm(
+            date=row["time"].date(),
+            location=row["geometry"],
+            temperature_min=row["t2m_min"],
+            temperature_mean=row["t2m_mean"],
+            temperature_max=row["t2m_max"],
+            dewpoint_temperature_min=row["d2m_min"],
+            dewpoint_temperature_mean=row["d2m_mean"],
+            dewpoint_temperature_max=row["d2m_max"],
+            east_wind_min=row["u10_min"],
+            east_wind_mean=row["u10_mean"],
+            east_wind_max=row["u10_max"],
+            north_wind_min=row["v10_min"],
+            north_wind_mean=row["v10_mean"],
+            north_wind_max=row["v10_max"],
+            leaf_area_index_high_vegetation_min=row["lai_hv_min"],
+            leaf_area_index_high_vegetation_mean=row["lai_hv_mean"],
+            leaf_area_index_high_vegetation_max=row["lai_hv_max"],
+            potential_evaporation_min=row["pev_min"],
+            potential_evaporation_mean=row["pev_mean"],
+            potential_evaporation_max=row["pev_max"],
+            surface_latent_heat_flux_min=row["slhf_min"],
+            surface_latent_heat_flux_mean=row["slhf_mean"],
+            surface_latent_heat_flux_max=row["slhf_max"],
+            surface_net_solar_radiation_min=row["ssr_min"],
+            surface_net_solar_radiation_mean=row["ssr_mean"],
+            surface_net_solar_radiation_max=row["ssr_max"],
+            surface_net_thermal_radiation_min=row["str_min"],
+            surface_net_thermal_radiation_mean=row["str_mean"],
+            surface_net_thermal_radiation_max=row["str_max"],
+            surface_sensible_heat_flux_min=row["ssr_min"],
+            surface_sensible_heat_flux_mean=row["ssr_mean"],
+            surface_sensible_heat_flux_max=row["ssr_max"],
+            total_precipitation_min=row["tp_min"],
+            total_precipitation_mean=row["tp_mean"],
+            total_precipitation_max=row["tp_max"],
+            total_evaporation_min=row["e_min"],
+            total_evaporation_mean=row["e_mean"],
+            total_evaporation_max=row["e_max"],
+        )
+        for row in rows
+    ]
+    return daily_norms
--- postgis_era5/read.py
+++ postgis_era5/read.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
 from postgis_era5.types import WGS84Point
+from postgis_era5.parsing import parse_daily_norm
 
 db_string = "postgresql://localhost/era5"
 db_connection = create_engine(db_string)
@@ -62,7 +63,7 @@ class PSQLInterface:
         )
         with self.engine.connect() as conn:
             res = conn.execute(query, month=month, closest_geo=closest_point).fetchall()
-        return res
+        return parse_daily_norm(res)
 
     def retrieve_historical_month_year(
         self, var: str, month: int, location: WGS84Point, year=int
@@ -93,5 +94,6 @@ db.check_connection()
 res = db.retrieve_norm(
     var="t2m_min", month=5, location=WGS84Point(latitude=-22.804, longitude=-54.383)
 )
-print(res)
-print(res[0].keys())
+print(len(res))
+# print(res)
+# print(res[0].keys())
